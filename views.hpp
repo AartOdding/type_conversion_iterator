aa504ea4bb3988b074fd5561ee1243bf854c9568
Aart <aart_odding@hotmail.com>
2020-10-09 18:26:23 +0200

    Added reference_view and const_reference_view

--- views.hpp
+++ views.hpp
@@ -57,3 +57,58 @@ public:
         return m_end;
     }
 };
+
+template<typename Container>
+class reference_view
+{
+    using iterator = type_conversion_iterator<typename Container::iterator, 
+        reference_conversion<typename Container::value_type>>;
+    
+    iterator m_begin;
+    iterator m_end;
+
+public:
+
+    reference_view(Container& container)
+        : m_begin(container.begin())
+        , m_end(container.end())
+    { }
+
+    iterator begin() const
+    {
+        return m_begin;
+    }
+
+    iterator end() const
+    {
+        return m_end;
+    }
+};
+
+template<typename Container>
+class const_reference_view
+{
+    using iterator = type_conversion_iterator<typename Container::const_iterator, 
+        const_reference_conversion<typename Container::value_type>>;
+
+    iterator m_begin;
+    iterator m_end;
+
+public:
+
+    const_reference_view(const Container& container)
+        : m_begin(container.begin())
+        , m_end(container.end())
+    { }
+
+    iterator begin() const
+    {
+        return m_begin;
+    }
+
+    iterator end() const
+    {
+        return m_end;
+    }
+};
+
